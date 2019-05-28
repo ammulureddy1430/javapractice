ec51b90ef51bc2149ad3c98d799db79a033e053e
arun <arunkumarreddy.n@cognizant.com>
2019-05-28 11:06:10 +0530

    additionprogram

---
+++ javaexample1/src/javaexample1/Javaexample1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample1;
+
+/**
+ *
+ * @author home
+ */
+public class Javaexample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10,b=5;
+        int c;
+        c=a+b;
+        System.out.println("addition="+c);
+    }
+    
+}
